fa14130398f5660924f21fe91dbd1f70e53fd588
Christian Rodemeyer <atombrenner@gmail.com>
2021-01-27 18:33:52 +0100

    add some minimal animation

--- doc/research.md
+++ doc/research.md
@@ -2,3 +2,4 @@
 
 https://tailwindcss.com/
 https://css-tricks.com/seamless-responsive-photo-grid/
+https://github.com/wiringbits/my-photo-timeline/tree/master/src/main/scala/net/wiringbits/myphototimeline
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { h } from 'preact'
-import { useState, useEffect } from 'preact/hooks'
+import { h, createRef } from 'preact'
+import { useState, useEffect, useRef } from 'preact/hooks'
 import { images } from './image-data'
 import './App.css'
 import { assert } from 'chai'
 
-function nextImage(index: number, step: number, count: number) {
+function stepImage(index: number, step: number, count: number) {
   assert(step < count)
   return (index + step + count) % count
 }
 
+// const next = (index: number) => stepImage(index, 1, images.length)
+// const prev = (index: number) => stepImage(index, -1, images.length)
+
 function App() {
-  const [imageIndex, setImageIndex] = useState(0)
+  const [current, setCurrent] = useState(0)
+  const next = stepImage(current, 1, images.length)
+  const prev = stepImage(current, -1, images.length)
+
+  const ref = createRef()
+  useEffect(() => ref.current.focus())
+
+  const ref1 = useRef<HTMLDivElement>()
+  const ref2 = useRef<HTMLDivElement>()
 
   const handleKeyDown = (e: KeyboardEvent) => {
     switch (e.code) {
       case 'ArrowRight':
-        setImageIndex(nextImage(imageIndex, 1, images.length))
+        setCurrent(next)
         break
       case 'ArrowLeft':
-        setImageIndex(nextImage(imageIndex, -1, images.length))
+        setCurrent(prev)
         break
       default:
         return
@@ -28,11 +39,16 @@ function App() {
   }
 
   return (
-    <div tabIndex={-1} className="App" onKeyDown={handleKeyDown} class="App-container">
-      <div class="zoom">
-        <img class="image" src={images[imageIndex]} />
+    <div ref={ref} tabIndex={-1} className="App" onKeyDown={handleKeyDown} class="App-container">
+      <div key={prev} ref={ref1} class="photo photo-prev">
+        <img class="photo-image" src={images[prev]} />
       </div>
-      {/* <img class="image" src={images[imageIndex]} /> */}
+      <div key={current} ref={ref1} class="photo photo-current">
+        <img class="photo-image" src={images[current]} />
+      </div>
+      <div key={next} ref={ref2} class="photo photo-next">
+        <img class="photo-image" src={images[next]} />
+      </div>
     </div>
   )
 }
