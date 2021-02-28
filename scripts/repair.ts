9b50c6e3c75b1db8dba7bf9ed8027e460b3dd73e
Christian Rodemeyer <atombrenner@gmail.com>
2021-02-28 11:30:02 +0100

    convert non mp4 files

--- scripts/repair.ts
+++ scripts/repair.ts
@@ -1,4 +1,4 @@
-import { fstat, statSync, readFileSync } from 'fs-extra'
+import { fstat, statSync, readFileSync, removeSync } from 'fs-extra'
 import { basename, dirname, join } from 'path'
 import { readPhotoCreationDate, readFiles, readVideoCreationDate } from './read'
 import { format, parseISO, addSeconds, addHours } from 'date-fns'
@@ -26,25 +26,46 @@ async function findVideosWithoutCreationDate() {
   }
 }
 
-// repair photos with missing creation date
-async function repair() {
+// convert and add creationDate
+async function convertVideoAndAddCreationDate() {
   const files = readFileSync('video-without-date.txt').toString().split('\n')
   const filesWithDate = files
     .filter((f) => f.length > 1)
     .map((file, i) => {
       const year = Number.parseInt(basename(dirname(file)))
       const name = basename(file)
-
       const match = name.match(/\d{4}-\d{2}-\d{2}/) // try to parse a date from filename
       const date = match ? parseISO(match[0] + 'T06:00:00Z') : new Date(year, 0, 1, 6, 0, 0)
       const dateWithOffset = addSeconds(date, i).toISOString()
-
       const mp4file = '/home/christian/converted/' + name.substr(0, name.length - 3) + 'mp4'
-
       return `ffmpeg -i "${file}" -metadata creation_time=${dateWithOffset} "${mp4file}"`
     })
 
   filesWithDate.forEach((f) => console.log(f))
 }
+
+async function findNonMp4Files() {
+  const files = await readFiles('/home/christian/Data/MyMedia/Videos', videoPattern)
+  files
+    .filter((f) => !f.includes('unorganized') && !f.toLowerCase().endsWith('.mp4'))
+    .forEach((f) => {
+      console.log(f)
+    })
+}
+
+async function convertNonMp4FilesWithMetadata() {
+  const files = readFileSync('non-mp4-videos.txt').toString().split('\n')
+  files
+    .filter((f) => f.length > 1)
+    .map((file) => {
+      const name = basename(file)
+      const mp4File = '/home/christian/converted2/' + name.substr(0, name.length - 3) + 'mp4'
+      return `ffmpeg -i "${file}" -map_metadata 0 "${mp4File}"`
+    })
+    .forEach((f) => console.log(f))
+}
+
+// repair photos with missing creation date
+async function repair() {}
 
 repair().catch(console.error)
