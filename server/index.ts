0210b90be6e16f5a29722ca664be58121ec8c33b
Christian Rodemeyer <atombrenner@gmail.com>
2021-05-15 15:30:48 +0200

    log server side rotation

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import cors from '@koa/cors'
+import { appendFile } from 'fs-extra'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import serveStatic from 'koa-static'
@@ -10,12 +11,13 @@ const app = new Koa()
 app.use(cors()) // necessary for development because localhost:3000 is a different origin than localhost:9000
 app.use(bodyParser())
 app.use(async (ctx, next) => {
+  const path = decodeURIComponent(ctx.URL.pathname)
   if (ctx.method === 'DELETE') {
     ctx.body = 'OK'
     console.log(decodeURIComponent(ctx.URL.pathname))
   } else if (ctx.method === 'POST') {
+    rotate(path, ctx.request.body.rotation ?? 0)
     ctx.body = 'OK'
-    console.log(JSON.stringify(ctx.request.body))
   } else {
     await next()
   }
@@ -23,3 +25,8 @@ app.use(async (ctx, next) => {
 app.use(serveStatic(photos, { maxAge: 3600000 }))
 app.use(serveStatic(__dirname))
 app.listen(9000)
+
+function rotate(path: string, rotation: number) {
+  console.log('rotate', path, rotation)
+  return appendFile(join(photos, 'rotated.txt'), `${path}\n`)
+}
