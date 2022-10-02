511326ca1c7efc254a36c50c591f20592f32b875
Christian Rodemeyer <atombrenner@gmail.com>
2022-10-02 14:55:11 +0200

    update dependencies, fix jest dependency

--- webapp/app.test.tsx
+++ webapp/app.test.tsx
@@ -1,20 +1,21 @@
 /* jest-environment needs multiline comment
   @jest-environment jsdom
 */
-import { mocked } from '@atombrenner/jest-ts-utils'
 import { render, screen, waitFor } from '@testing-library/preact'
 import { h } from 'preact'
 import { App } from './app'
 import { loadPhotos } from './backend'
 
 jest.mock('./backend')
-mocked(loadPhotos).mockResolvedValue([
-  '2000/2000-01-01 001.jpg',
-  '2000/2000-01-02 002.jpg',
-  '2000/2000-01-03 003.jpg',
-  '2000/2000-02-04 001.jpg',
-  '2000/2000-02-05 002.jpg',
-])
+jest
+  .mocked(loadPhotos)
+  .mockResolvedValue([
+    '2000/2000-01-01 001.jpg',
+    '2000/2000-01-02 002.jpg',
+    '2000/2000-01-03 003.jpg',
+    '2000/2000-02-04 001.jpg',
+    '2000/2000-02-05 002.jpg',
+  ])
 
 describe('<App> with more than three images', () => {
   it('should render a photo', async () => {
