65a558b43a5e6781d289f609755e5e524bc17fb7
Christian Rodemeyer <atombrenner@gmail.com>
2023-04-15 16:18:25 +0200

    refactor read functions

--- scripts/read.spec.ts
+++ scripts/read.spec.ts
@@ -1,14 +1,48 @@
-import { mkdirsSync } from 'fs-extra'
-import { readFolders } from './read'
+import { mkdirSync, writeFileSync, mkdtempSync, rmSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { readFiles, readFolders } from './read'
+
+const tmp = mkdtempSync(join(tmpdir(), 'read.spec-'))
+afterEach(() => {
+  rmSync(tmp, { force: true, recursive: true })
+})
+const createTmpFile = (...paths: string[]) => {
+  const file = paths.pop()
+  if (!file) throw Error('no tmp file path')
+  mkdirSync(join(tmp, ...paths), { recursive: true })
+  const path = join(tmp, ...paths, file)
+  writeFileSync(path, '')
+  return path
+}
 
 describe('readFolders', () => {
   it('should return only top level folders', async () => {
-    const tmp = '/tmp/read.spect.ts'
-    mkdirsSync(tmp + '/bla')
-    mkdirsSync(tmp + '/bli/bla/blub')
-    mkdirsSync(tmp + '/hulle')
+    mkdirSync(join(tmp, 'a'), { recursive: true })
+    mkdirSync(join(tmp, 'b', 'bb', 'bbb'), { recursive: true })
+    mkdirSync(join(tmp, 'c'), { recursive: true })
 
     const folders = await readFolders(tmp)
-    expect(folders.sort()).toEqual(['/bla', '/bli', '/hulle'].map((f) => tmp + f))
+    expect(folders.sort()).toEqual(['a', 'b', 'c'].map((f) => join(tmp, f)))
+  })
+})
+
+describe('readFiles', () => {
+  it('should recursively read all files that match the given pattern', async () => {
+    const expectedFiles = []
+    createTmpFile('.ignore', 'no.jpeg')
+    createTmpFile('_ignore_', 'no.jpeg')
+    createTmpFile('t.txt')
+    expectedFiles.push(createTmpFile('f1.jpeg'))
+    expectedFiles.push(createTmpFile('f2.jpeg'))
+    expectedFiles.push(createTmpFile('a', 'f4.jpeg'))
+    expectedFiles.push(createTmpFile('a', 'aa', 'aaa', 'f3.jpeg'))
+    expectedFiles.push(createTmpFile('a', 'bb', 'f5.jpeg'))
+    expectedFiles.push(createTmpFile('a', 'bb', 'f6.jpeg'))
+    createTmpFile('a', 'bb', 't.txt')
+    mkdirSync(join(tmp, 'a', 'bb', 'bbb'), { recursive: true }) // empty leave folder
+
+    const files = await readFiles(tmp, /.*\.jpeg$/)
+    expect(expectedFiles.sort()).toEqual(files.sort())
   })
 })
--- scripts/read.ts
+++ scripts/read.ts
@@ -1,29 +1,36 @@
+import { join } from 'node:path'
+import { readdir } from 'node:fs/promises'
+
 import parseExif, { Exif } from 'exif-reader'
-import { readdir, stat } from 'fs-extra'
-import { join } from 'path'
 import sharp from 'sharp'
 import { ffprobe } from './ffprobe'
 
-// ignore folders starting with a dot or are surrounded with underscores
+// ignore folders that start with a dot or are surrounded with underscores
 const ignoredFolders = /^((\..*)|(_.*_))$/
 
-// read all file names from a folder and all subfolders recusively
+// recursively read all file names that match a pattern from a folder
 export async function readFiles(folder: string, pattern: RegExp): Promise<string[]> {
-  const entries = await readdir(folder, { withFileTypes: true })
-  const folders: string[] = []
   const files: string[] = []
-  for (const entry of entries) {
-    if (entry.isDirectory()) {
-      if (!ignoredFolders.test(entry.name)) folders.push(join(folder, entry.name))
-    } else if (pattern.test(entry.name)) {
-      files.push(join(folder, entry.name))
+
+  const innerReadFiles = async (folder: string) => {
+    const entries = await readdir(folder, { withFileTypes: true })
+    const folders: string[] = []
+    for (const entry of entries) {
+      if (entry.isDirectory()) {
+        if (ignoredFolders.test(entry.name)) continue
+        folders.push(join(folder, entry.name))
+      } else if (pattern.test(entry.name)) {
+        files.push(join(folder, entry.name))
+      }
     }
+    await Promise.all(folders.map((f) => innerReadFiles(f)))
   }
-  const moreFiles = await Promise.all(folders.map((f) => readFiles(f, pattern)))
-  return [...files, ...moreFiles.flat()]
+
+  await innerReadFiles(folder)
+  return files
 }
 
-/** return all folders in folder */
+// read all folders in folder */
 export async function readFolders(folder: string): Promise<string[]> {
   const entries = await readdir(folder, { withFileTypes: true })
   return entries
@@ -31,14 +38,9 @@ export async function readFolders(folder: string): Promise<string[]> {
     .map((e) => join(folder, e.name))
 }
 
-export async function readStats(path: string) {
-  const stats = await stat(path)
-  return { size: stats.size, modified: stats.mtimeMs }
-}
-
 export type ReadCreationDate = (path: string) => Promise<number>
 
-export async function readPhotoCreationDate(path: string): Promise<number> {
+export const readPhotoCreationDate: ReadCreationDate = async (path) => {
   const throwIfUndefined = <T>(v: T | undefined | null): T => {
     if (!v) throw Error('Cannot get image creation date for ' + path)
     return v
@@ -54,7 +56,7 @@ export async function readPhotoCreationDate(path: string): Promise<number> {
   return sharp(path).metadata().then(pickExif).then(parseExif).then(pickCreationDate)
 }
 
-export async function readVideoCreationDate(path: string): Promise<number> {
+export const readVideoCreationDate: ReadCreationDate = async (path) => {
   // ideally get it from mp4 container
   const { created } = await ffprobe(path) // works for avi, mp4, mov
   if (!isNaN(created)) return created
