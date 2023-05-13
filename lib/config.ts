18e44824c61c7aad5e7368eec6c707ff8f008671
Christian Rodemeyer <atombrenner@gmail.com>
2023-05-13 17:10:11 +0200

    deploy app bundle

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,13 @@
+import { existsSync } from 'node:fs'
 import { join } from 'node:path'
 
-export const mediaRootPath = '/home/christian/Data/MyMedia'
+const myMedia = process.env.MY_MEDIA
+if (!myMedia || !existsSync(myMedia)) {
+  console.error('MY_MEDIA environment variable is not defined')
+  process.exit(1)
+}
+
+export const mediaRootPath = myMedia
 export const photoRootPath = join(mediaRootPath, 'Photos')
 export const videoRootPath = join(mediaRootPath, 'Videos')
 
--- server/index.ts
+++ server/index.ts
@@ -3,38 +3,36 @@ import { appendFile, mkdirsSync, move, readJSON, writeJSON } from 'fs-extra'
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import serveStatic from 'koa-static'
-import { join, basename } from 'path'
+import { mediaRootPath, photoRootPath } from 'lib/config'
 import { makePhotoPathName } from 'lib/names'
-import { photoRootPath } from 'lib/config'
+import { basename, join } from 'path'
 
-const photos = process.env.PHOTOS ?? photoRootPath
-const deletedPhotos = join(photos, '_deleted_')
+const deletedPhotos = join(photoRootPath, '_deleted_')
 mkdirsSync(deletedPhotos)
 
 const app = new Koa()
 app.use(cors()) // necessary for development because localhost:3000 is a different origin than localhost:9000
 app.use(bodyParser())
-app.use(async (ctx, next) => {
-  if (ctx.path === '/index.json') return await next()
 
+app.use(async (ctx, next) => {
   const timestamp = Number.parseFloat(ctx.path.substring(1))
   if (isNaN(timestamp)) {
-    ctx.status = 404
-    return
-  }
-  ctx.path = makePhotoPathName(timestamp)
-  if (ctx.method === 'DELETE') {
-    deletePhoto(ctx.path, timestamp)
-    ctx.body = 'OK'
-  } else if (ctx.method === 'POST') {
-    rotatePhoto(ctx.path, ctx.request.body)
-    ctx.body = 'OK'
-  } else {
     await next()
+  } else {
+    ctx.path = makePhotoPathName(timestamp)
+    if (ctx.method === 'DELETE') {
+      deletePhoto(ctx.path, timestamp)
+      ctx.body = 'OK'
+    } else if (ctx.method === 'POST') {
+      rotatePhoto(ctx.path, ctx.request.body)
+      ctx.body = 'OK'
+    } else {
+      await next()
+    }
   }
 })
-app.use(serveStatic(photos, { maxAge: 5 * 60 * 1000 }))
-app.use(serveStatic(__dirname))
+app.use(serveStatic(photoRootPath, { maxAge: 5 * 60 * 1000 }))
+app.use(serveStatic(join(mediaRootPath, 'photo-timeline')))
 app.onerror = (err: Error) => {
   // ignore ECONNRESET errors that happen if browser cancels download, e.g. when skipping fast through photos
   if (!err.message.includes('ECONNRESET')) console.error(err)
@@ -44,14 +42,14 @@ app.listen(9000)
 function rotatePhoto(path: string, body: any) {
   const rotation = body.rotation ?? 0
   console.log('rotate', path, rotation)
-  return appendFile(join(photos, 'rotated.txt'), `${path}\n`)
+  return appendFile(join(photoRootPath, 'rotated.txt'), `${path}\n`)
 }
 
 async function deletePhoto(path: string, timestamp: number) {
-  move(join(photos, path), join(photos, '_deleted_', basename(path)))
+  move(join(photoRootPath, path), join(photoRootPath, '_deleted_', basename(path)))
 
   // update index
-  const indexFileName = join(photos, 'index.json')
+  const indexFileName = join(photoRootPath, 'index.json')
   const index: number[] = await readJSON(indexFileName)
   await writeJSON(
     indexFileName,
